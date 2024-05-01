d666bbd6e1a519fc68dfe2278bba3641f7837899
g3ngi <kenzienabeel@hotmail.com>
2024-05-01 11:54:00 +0700

    add docker config and model

---
+++ real_estate_advertising/extra-addons/estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Real_Estate_Advertising",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Kenzie Nabeel",
+    'category': 'estate',
+    'description': """
+    Real Estate Advertising 
+    """,
+    # 'data': [
+    #     'views/mymodule_view.xml',
+    # ],
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+    'installable': 'True',
+    'application': 'True',
+    'license': 'LGPL-3',
+}
